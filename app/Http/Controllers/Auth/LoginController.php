0a1503d8174bdaba0defbe6873f76a57d976ca9a
cperko <cperko@teachers-teachers.com>
2017-04-17 10:43:08 -0500

    Added new page for logged in users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/myBook';
 
     /**
      * Create a new controller instance.
