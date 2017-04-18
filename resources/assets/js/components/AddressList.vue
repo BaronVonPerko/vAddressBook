3711b72d29af0d730b6c7a1a6a65a0f1844c7618
cperko <cperko@teachers-teachers.com>
2017-04-18 16:15:48 -0500

    Loading message

--- resources/assets/js/components/AddressList.vue
+++ resources/assets/js/components/AddressList.vue
@@ -1,23 +1,27 @@
 <template>
     <div>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Phone</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="person in people">
-                    <td>{{person.first_name}}</td>
-                    <td>{{person.last_name}}</td>
-                    <td>{{person.phone}}</td>
-                    <td>{{person.email}}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div v-if="loaded">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="person in people">
+                        <td>{{person.first_name}}</td>
+                        <td>{{person.last_name}}</td>
+                        <td>{{person.phone}}</td>
+                        <td>{{person.email}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div v-if="!loaded" class="loading-message">Loading... Please Wait</div>
     </div>
 </template>
 
@@ -25,15 +29,24 @@
     export default {
         data() {
             return {
-                people: []
+                people: [],
+                loaded: false
             };
         },
 
         mounted() {
             this.$http.get('/api/address').then(function(res) {
                 this.people = res.body;
-                console.log(this.people);
+                this.loaded = true;
             });
         }
     }
 </script>
+
+
+<style scoped>
+.loading-message {
+    text-align: center;
+    margin: 50px auto;
+}
+</style>
